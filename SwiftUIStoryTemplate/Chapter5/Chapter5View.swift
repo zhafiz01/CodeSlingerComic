10e5c5526ad97f02a25762a61ac66734e9e34318
Jacinta <jacintachevon@gmail.com>
2023-11-17 08:04:15 -0500

    Jacinta FInal

--- SwiftUIStoryTemplate/Chapter5/Chapter5View.swift
+++ SwiftUIStoryTemplate/Chapter5/Chapter5View.swift
@@ -3,6 +3,16 @@ import SwiftUI
 struct Chapter5View: View {
     var body: some View {
         Text("Chapter 5")
+        
+                        
+            Color .black
+            .ignoresSafeArea()
+        
+        Image(.lilywins)
+            .resizable()
+            .scaledToFit()
+            
+                        
     }
 }
 
